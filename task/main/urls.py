be96f6da6ab36b2553ff57e336a80a81343fea45
Orwall46 <Artashes46@yandex.ru>
2022-12-12 21:20:29 +0500

    edited urls

--- task/main/urls.py
+++ task/main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import item_view, ItemListView
 
 
@@ -6,3 +8,5 @@ urlpatterns = [
     path('item/<int:pk>', item_view, name='detail_item'),
     path('', ItemListView.as_view(), name='home')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
